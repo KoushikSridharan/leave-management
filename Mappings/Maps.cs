b36db1d4495746e34207d9964c5e90f1a54207a0
KoushikSridharan <sidakoushik@gmail.com>
2020-06-08 14:46:37 +0530

    Leave Management Part1 Code

---
+++ Mappings/Maps.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Models;
+using leave_management.Data;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>();
+            CreateMap<LeaveType, CreateDetailsTypeVM>();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>();
+            CreateMap<LeaveHistory, LeaveHistoryVM>();
+            CreateMap<Employee, EmployeeVM>();
+        }
+        
+        
+        
+        
+    
+    
+    
+    }
+}
---
+++ Models/LeaveTypeVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class DetailsLeaveTypeVM
+    {
+        public int id { get; set; }
+        
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateDetailsTypeVM
+    {        
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
